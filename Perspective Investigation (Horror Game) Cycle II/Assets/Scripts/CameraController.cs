2e5f6350b4edaf11f7a110bb0723c045b769b0a3
Sachta291 <476736@chrome.escg.ac.uk>
2019-11-22 14:41:52 +0000

    player movement, and camera position

---
+++ Perspective Investigation (Horror Game) Cycle II/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    void Start()
+    {
+        //player x - camera x = 0
+       
+
+        offset = new Vector3(transform.position.x - player.transform.position.x, transform.position.y - player.transform.position.y, transform.position.z - player.transform.position.z);
+
+
+    }
+        void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+}
