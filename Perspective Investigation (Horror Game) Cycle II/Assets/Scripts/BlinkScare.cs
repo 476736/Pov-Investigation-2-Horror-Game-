c6dc0c12285be921780ec23569c332fe2b507112
Sachta291 <476736@chrome.escg.ac.uk>
2019-11-29 14:40:51 +0000

    lighting and triggers

---
+++ Perspective Investigation (Horror Game) Cycle II/Assets/Scripts/BlinkScare.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkScare : MonoBehaviour
+{
+    public GameObject scareTrigger;
+    public GameObject jumpCube;
+
+    private void Start()
+    {
+        jumpCube.gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider scareTrigger)
+    {
+        jumpCube.gameObject.SetActive(true);
+        OffAndOn();
+    }
+    IEnumerator OffAndOn()
+    {
+        jumpCube.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+        jumpCube.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ Perspective Investigation (Horror Game) Cycle II/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+
+public class MouseHover : MonoBehaviour, IPointerEnterHandler
+{
+    public Text NameOfGame;
+    public Text Start;
+    public Text Quit;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Start.color = Color.white;
+        Quit.color = Color.white;
+    }
+}
