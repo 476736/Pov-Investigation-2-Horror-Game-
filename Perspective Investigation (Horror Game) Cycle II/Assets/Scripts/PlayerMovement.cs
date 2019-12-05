7864d932bd588dff52202ae55a8563430a092c14
Sachta291 <476736@chrome.escg.ac.uk>
2019-12-05 15:22:45 +0000

    Began to add questionnaire trigger

--- Perspective Investigation (Horror Game) Cycle II/Assets/Scripts/PlayerMovement.cs
+++ Perspective Investigation (Horror Game) Cycle II/Assets/Scripts/PlayerMovement.cs
@@ -24,11 +24,11 @@ public class PlayerMovement : MonoBehaviour
 
     //private void Jumping()
     //{
-        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        //{
-            //selfRigidbody.AddForce(0, jumpConst, 0, ForceMode.Impulse);
-            //isGrounded = false;
-        //}
+    //if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+    //{
+    //selfRigidbody.AddForce(0, jumpConst, 0, ForceMode.Impulse);
+    //isGrounded = false;
+    //}
     //}
 
     private void MovementFunc()
@@ -49,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
 
     //void OnCollisionEnter(Collision collision)
     //{
-        //isGrounded = true;
+    //isGrounded = true;
     //}
 }
 
